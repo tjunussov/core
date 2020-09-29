6cdd4b72b27928d916a7afd3bfbc0de5ddca3365
Chris Talkington <chris@talkingtontech.com>
2020-09-28 22:29:29 -0500

    Update __init__.py

--- homeassistant/components/canary/__init__.py
+++ homeassistant/components/canary/__init__.py
@@ -94,7 +94,7 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         _LOGGER.error("Unable to connect to Canary service: %s", str(error))
         raise ConfigEntryNotReady from error
 
-    coordinator = CanaryDataUpdateCoordinator(api=canary_api)
+    coordinator = CanaryDataUpdateCoordinator(hass, api=canary_api)
     await coordinator.async_refresh()
 
     if not coordinator.last_update_success:
